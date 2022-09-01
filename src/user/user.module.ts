df87aaf19fa24e6cbebb58032aa8d8e77d3262f6
Arlyson Mathias <mathiassouza10@outlook.com>
2022-09-01 12:16:45 -0300

    feature: implemented AuthModel, also implemented login

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from 'src/auth/jwt.estrategy';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtStrategy],
 })
 export class UserModule {}
